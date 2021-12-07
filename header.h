41ae5584ce119a6f46dd57f85e444b59c1a48333
Kriz Royce A. Tahimic <62783701+KrizTahimic@users.noreply.github.com>
2021-12-07 20:06:35 +0800

    Created header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+String create();
+Set add (String s, char c);
+String insert (String s, char c, int pos);
+String delete (String s, int pos);
+String substitute (String s, String find, String replace);
+int compare (String s1, String s2);
+int getLength(String s);
+void print (String s);  
