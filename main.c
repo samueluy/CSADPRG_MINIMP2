0eafe8bb42b02d63242870f0ced07718e61eba32
SamuelUy <samuel_uy@dlsu.edu.ph>
2022-01-10 09:32:16 +0800

    fixed compare

--- main.c
+++ main.c
@@ -32,22 +32,19 @@ int main (void)
     add (s3, 'l');
     add (s3, 'd');
     
-    add (s3, 'C');
-    add (s3, 'C');
-    add (s3, 'P');
-    add (s3, 'R');
-    add (s3, 'O');
-    add (s3, 'G');
-    
+    add (s4, 'C');
+    add (s4, 'C');
+    add (s4, 'P');
+    add (s4, 'R');
+    add (s4, 'O');
+    add (s4, 'G');
+
     print (s1);   // this statement will display (Hello)
     add (s2, '!');
     print (s2); // this statement will display (Hello World!)
     //print (insert(s2, 11, 's')); // this statement will return (Hello Worlds!)
     //print (delete(s2, 11)); // this statement will return (Hello World)
-
-    
-    
-    
+ 
     //print (substitute(s2, s3, s4)); // this statement will return (Hello CCPROG!)
     //print (substitute(s2, "World", "CCPROG")); // this statement will return (Hello CCPROG!)
     compare(s1, s2); // this statement will return (0)
@@ -119,78 +116,13 @@ void print(String str)
  */
  String substitute(String s, String find, String replace)
  {
- 	int nCount, findLen, simCount=0, found=0;
- 	findLen = getLength(find);
- 	String s1Current = s;
- 	String s2Current = find; // default value
- 	String s3Current = replace;
- 	int lengthOfFind = getLength(find);
- 	String newString;
- 	String newTemp = s;
- 	newString = create();
- 	
-    while(s1Current != NULL){ // itterate each character of s
-    printf("here");
-    	if(s1Current == s2Current){ // if first letter is similar
-    		while(s2Current != NULL){ // itterate through each character of find
-    		
-    			if(s1Current != s2Current) // exit loop because different
-				{
-					nCount++; // start from here
-					simCount=0; // reset similarity counter
-					break; // exit the loop
-				}
-    			else // proceed to next character
-				{
-					if(simCount==findLen) // check if text is already similar
-					{ 
-						nCount-=findLen; // character count till the start of the word
-						found=1; 
-						break; // exit loop
-					}
-    				s1Current = s1Current->strNext; 
-	    			s2Current = s2Current->strNext;
-		 			nCount++;
-				}
-			}
-	 		
-			break;
-		 }
-	 	else{ // next character
-	 		s2Current = find; // reset
-	 		s1Current = s1Current->strNext;
-	 		nCount++;
-		 }
-		 
-		 if(found)
-		 {
-		 	while(newTemp->strNext != NULL)
-		 	{
-		 		if(nCount==0) // in first character of find
-				 {
-				 	while(s3Current != NULL) // itterate for the length of replace
-				 	{
-		 			add(newString, s3Current->cChar);
-		 			s3Current = s3Current->strNext; // next character
-		 			}
-		 			while(lengthOfFind != 0)
-		 			{
-		 				newTemp = newTemp->strNext; // move to the end of find
-					}
-					nCount=-1; // exit if condition
-				 }
-		 		add(newString, newTemp->cChar); // add current char to newString
-		 		newTemp = newTemp->strNext; // next character of newTemp
-		 		nCount--; //
-			}
-		 }
-		 else // no similarities found
-		 {
-		 	printf("Error! Substitution couldn�t be performed\n");
-		 }
-	}
-	 
-    return newString;
+	/*
+	create a temp string -> loop till " "
+	after " ", we compare if similar with String find
+	if similar, insert String replace. -> we count the spaces and if == we do insert(), mmem then just print the rest
+	else, Error
+	*/
+    return s;
  }
 
 /**
@@ -203,20 +135,30 @@ void print(String str)
  
  void compare(String s1, String s2)
  {
-	String s1Current = s1;
+ 	
+	String s1Current;
 	String s2Current = s2;
+	s1Current = create();
+	
+	while(s1 != NULL) // create a temporarry String which does not contain any special character
+	{
+		if((s1->cChar >= 65 && s1->cChar <= 90) || (s1->cChar >= 97 && s1->cChar <= 122))
+			add(s1Current, s1->cChar);
+
+		s1 = s1->strNext;
+	}
+	
 	
  	// Check if the lenghts are the same
- 	 if(getLength(s1) != getLength(s2)){
+ 	 if(getLength(s1Current) != getLength(s2)){
  	 	printf("0");
  	 	return;
 	  }
  		
  	// Chech the equivalence of each character
- 	
  	while(s1Current != NULL)
 	 {
-	 	if(s1Current!=s2Current){
+	 	if(s1Current->cChar!=s2Current->cChar){
 	 		printf("0");
 			return;
 		 }
